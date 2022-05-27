70aef0d373334599c72994b1b83bb5835c489bae
Ahzam Akhtar <ahzam8186@gmail.com>
2022-05-28 03:25:53 +0530

    final changes

--- app/src/main/java/com/example/android/news_recycle/MainActivity.java
+++ app/src/main/java/com/example/android/news_recycle/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.android.news_recycle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,30 +26,90 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<News> newsArray = new ArrayList<>();
+    RecyclerView recyclerView;
+    NewsListAdapter newsListAdapter;
+    String url = "https://gnews.io/api/v4/top-headlines?token=a5aaa59810d58657fc9d4f7d6c890ca6&lang=en&country=in&topic=nation";
+    public void changeurls(View view){
+        //Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show();
+         url = "https://gnews.io/api/v4/top-headlines?token=a5aaa59810d58657fc9d4f7d6c890ca6&lang=en&country=in&topic=sports";
+         newsArray.clear();
+        fetchApi();
+        newsListAdapter.notifyDataSetChanged();
+
+
+    }
+    public void changeurle(View view){
+        //Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show();
+        url = "https://gnews.io/api/v4/top-headlines?token=a5aaa59810d58657fc9d4f7d6c890ca6&lang=en&country=in&topic=entertainment";
+        newsArray.clear();
+        fetchApi();
+        newsListAdapter.notifyDataSetChanged();
+    }
+    public void changeurlw(View view){
+        //Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show();
+        url = "https://gnews.io/api/v4/top-headlines?token=a5aaa59810d58657fc9d4f7d6c890ca6&lang=en&country=in&topic=world";
+        newsArray.clear();
+        fetchApi();
+        newsListAdapter.notifyDataSetChanged();
+    }
+    public void changeurlsc(View view){
+        //Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show();
+        url = "https://gnews.io/api/v4/top-headlines?token=a5aaa59810d58657fc9d4f7d6c890ca6&lang=en&country=in&topic=science";
+        newsArray.clear();
+        fetchApi();
+        newsListAdapter.notifyDataSetChanged();
+    }
+     //List<News> newsArray;
+
+     SwipeRefreshLayout swipeRefreshLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+
+         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+
+
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(linearLayoutManager);
-
-        NewsListAdapter newsListAdapter = new NewsListAdapter(MainActivity.this,newsArray);
+        recyclerView.setHasFixedSize(true);
+        //fetchApi();
+        newsListAdapter = new NewsListAdapter(MainActivity.this,newsArray);
         recyclerView.setAdapter(newsListAdapter);
-
         fetchApi();
+
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeup);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                fetchApi();
+                Toast.makeText(MainActivity.this, "Data Refreshed", Toast.LENGTH_SHORT).show();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+
     }
 
 
     public void fetchApi(){
+        ProgressBar prog = (ProgressBar) findViewById(R.id.prog);
+        prog.setVisibility(View.VISIBLE);
+        //prog.setVisibility(View.VISIBLE);
         //RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://gnews.io/api/v4/top-headlines?token=f1d8a882ca73d2f674a31e1339b923eb&lang=en&country=in&topic=nation";
+        //String url = "https://gnews.io/api/v4/top-headlines?token=a5aaa59810d58657fc9d4f7d6c890ca6&lang=en&country=in&topic=nation";
+        //String url = "https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=097575c06ecd4bc4b550fae79a2bd131";
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
@@ -57,11 +123,21 @@ public class MainActivity extends AppCompatActivity {
                         String description = jsonObject.getString("description");
                         String url= jsonObject.getString("url");
                         String image = jsonObject.getString("image");
+                        //JSONArray newjsonarray = jsonObject.getJSONArray("source");
+                        //String name = (String) jsonObject.getJSONObject("souce").get("name");
+                        //Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+                        //for(int j=0; j<newjsonarray.length(); j++){
+                            //JSONObject newjsonObject = jsonArray.getJSONObject(i);
+                            //String name = newjsonObject.getString("name");
+                            //Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+                        //}
                         News news = new News(title,description,url,image);
                         //Toast.makeText(MainActivity.this, title, Toast.LENGTH_SHORT).show();
                         newsArray.add(news);
+                        prog.setVisibility(View.GONE);
 
                     }
+                    newsListAdapter.notifyDataSetChanged();
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -77,5 +153,6 @@ public class MainActivity extends AppCompatActivity {
         MySingleton.getInstance(MainActivity.this).addToRequestQueue(jsonObjectRequest);
 
     }
+
 
 }
--- app/src/main/java/com/example/android/news_recycle/NewsListAdapter.java
+++ app/src/main/java/com/example/android/news_recycle/NewsListAdapter.java
@@ -1,22 +1,27 @@
 package com.example.android.news_recycle;
 
 import android.content.Context;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.browser.customtabs.CustomTabsIntent;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class NewsListAdapter  extends RecyclerView.Adapter<NewsListAdapter.MyViewHolder> {
-    ArrayList<News> newslist   = new ArrayList<>();
+    //ArrayList<News> newslist   = new ArrayList<>();
+    ArrayList<News> newslist;
    //ArrayList<News> newslist = new ArrayList<>()
     //ArrayList<News> newslist;
     Context context;
@@ -46,15 +51,27 @@ public class NewsListAdapter  extends RecyclerView.Adapter<NewsListAdapter.MyVie
     public int getItemCount() {
         return newslist.size();
     }
-    public class MyViewHolder extends RecyclerView.ViewHolder{
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView title;
         TextView description;
         ImageView image;
         public MyViewHolder(View itemView){
             super(itemView);
+            itemView.setOnClickListener(this);
             title = (TextView) itemView.findViewById(R.id.title_id);
             description = (TextView) itemView.findViewById(R.id.description_id);
             image = (ImageView) itemView.findViewById(R.id.testing);
+        }
+        public void onClick(View view){
+            int position = this.getAbsoluteAdapterPosition();
+            News newss = newslist.get(position);
+            //Toast.makeText(context, String.valueOf(position), Toast.LENGTH_SHORT).show();
+            String url = newss.getUrl();
+            CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+            CustomTabsIntent customTabsIntent = builder.build();
+            customTabsIntent.launchUrl(context, Uri.parse(url));
+
+
         }
     }
 }
